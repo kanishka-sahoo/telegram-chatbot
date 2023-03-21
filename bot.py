78e0c69ba8ce670667567d29bd1cb11f913a5732
kanishka-sahoo <kanishka.sa.hoo@gmail.com>
2023-03-21 20:41:05 +0530

    Added basic logging functionality

--- bot.py
+++ bot.py
@@ -6,19 +6,30 @@ import openai_interface as oif
 import requests
 import json
 from telegram.ext import *
+import logging
 
 load_dotenv()
 
 # initialise bot
 BOT_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
+
 oif.init_msg = """
 You are a conversational AI Chatbot specialising in positive user interaction. Pretend to be human wherever and whenever possible.
 
 If you are faced with a question you cannot or are not allowed to answer, reply as a human would.
 
-If asked about events outside your knowledge cutoff, or about recent events, just say that you don't know. If the content asked violates OpenAI policy, just simply decline to answer the question.
+If asked about events outside your knowledge cutoff, or about recent events, just say that you don't know. If the content asked violates OpenAI policy, just simply decline to answer the question, as a human.
 """
+
+
+# setting up logging
+logging.basicConfig(filename="messages.log",
+                    format='%(asctime)s %(message)s',
+                    filemode='w')
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 
 # Test bot online status
@@ -30,7 +41,7 @@ def net_check(message):
 # Get weather of given region
 @bot.message_handler(commands=['weather'])
 def getweatherinfo(message):
-    bot.reply_to(message, "Pong!")
+    bot.reply_to(message, "Coming Soon!")
 
 
 @bot.message_handler(commands=['clearchat'])
@@ -43,7 +54,7 @@ def clearchat(message):
 @bot.message_handler(func=lambda msg: True)
 def chat_gpt_complete(message):
     bot.reply_to(message, oif.get_response(message.text))
-    print(f'''{message.from_user.username}: {message.text}''')
+    logger.info(f'''<{message.from_user.username}>: {message.text}''')
 
 
 bot.infinity_polling()
