541d664a043646ffebe7a78f18727f5a80bf0344
kanishka-sahoo <kanishka.sa.hoo@gmail.com>
2023-03-22 20:30:39 +0530

    Added rejection of 'none' usernames and updated prompt.

--- bot.py
+++ bot.py
@@ -18,9 +18,14 @@ oif.invoke_key(os.environ.get('API_TOKEN'))
 init_msg = """
 You are a conversational AI Chatbot specialising in positive user interaction. Pretend to be human wherever and whenever possible.
 
-If you are faced with a question you cannot or are not allowed to answer, reply as a human would.
-
-If asked about events outside your knowledge cutoff, or about recent events, just say that you don't know. If the content asked violates OpenAI policy, just simply decline to answer the question, as a human.
+1) If you are faced with a question you cannot or are not allowed to answer, reply as a human would.
+2) If asked about events outside your knowledge cutoff, or about recent events, just say that you don't know. 
+3) If the content asked violates OpenAI policy, just simply decline to answer the question, as a human. 
+4) Try not to mention that you are AI or a language model.
+5) When asked for the following, reply as shown or similar:
+    user asks for the (date/time) -> the (date/time) is ${(date/time)}
+    user asks for the weather in given city -> the weather in given city is ${weather[city]}
+    user asks for other current info -> I don't have the current info.
 """
 
 
@@ -35,6 +40,8 @@ total_users_chats = []
 
 # handle the bot request pipeline
 def do_user_action(username):
+    if username == "none":
+        return 0xDEADBEEF
     for i in total_users_chats:
         if i and i.username == username:
             return i
@@ -61,17 +68,23 @@ def getweatherinfo(message):
 @bot.message_handler(commands=['clearchat'])
 def clearchat(message):
     ctx_user = do_user_action(str(message.from_user.username))
-    ctx_user.messages = []
-    bot.reply_to(message, 'Cleared Chat')
-    logger.info(f'''<{message.from_user.username}>: {message.text}''')
+    if ctx_user == 0xDEADBEEF:
+        bot.reply_to(message, 'Please add a username to your account for chat security.')
+    else:
+        ctx_user.messages = []
+        bot.reply_to(message, 'Cleared Chat')
+        logger.info(f'''<{message.from_user.username}>: {message.text}''')
 
 
 # Use ChatGPT for responses
 @bot.message_handler(func=lambda msg: True)
 def chat_gpt_complete(message):
     ctx_user = do_user_action(str(message.from_user.username))
-    bot.reply_to(message, ctx_user.get_response(message.text))
-    logger.info(f'''<{message.from_user.username}>: {message.text}''')
+    if ctx_user == 0xDEADBEEF:
+        bot.reply_to(messsage, 'Please add a username to your account for chat security.')
+    else:
+        bot.reply_to(message, ctx_user.get_response(message.text))
+        logger.info(f'''<{message.from_user.username}>: {message.text}''')
 
 
 bot.infinity_polling()
