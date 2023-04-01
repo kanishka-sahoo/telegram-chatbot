ca54c671535036de56683571f3de8012e9b03cab
kanishka-sahoo <kanishka.sa.hoo@gmail.com>
2023-04-01 17:33:50 +0530

    Changed up the code a bit

--- bot.py
+++ bot.py
@@ -3,9 +3,6 @@ import telebot
 import os
 from dotenv import load_dotenv
 import openai_interface as oif
-import requests
-import json
-from telegram.ext import *
 import logging
 import openai
 load_dotenv()
@@ -15,14 +12,15 @@ BOT_TOKEN = os.environ.get('TELEGRAM_TOKEN')
 bot = telebot.TeleBot(BOT_TOKEN)
 oif.invoke_key(os.environ.get('API_TOKEN'))
 
-init_msg = """
-You are a conversational AI Chatbot specialising in positive user interaction.
+INIT_MSG = """
+You are a conversational AI Chatbot called Assistant,
+specialising in positive user interaction.
 Pretend to be human wherever and whenever possible.
 
 Respond to technical, complex questions to the best of your abilities
 Refrain from calling a question complex, and try to solve it instead.
 """
-user_init_text = """
+USER_INIT_TEXT = """
 Welcome to Tele-GPT! Here are a few useful commands.
 /start : Brings up this message.
 /clearchat : clears the chat history, on OpenAI's servers
@@ -50,18 +48,19 @@ total_users_chats = []
 # Handle the case where username is not defined
 def no_username(bot, message):
     bot.reply_to(
-        message, 'Please add a username to your account via the Telegram settings for chat privacy and security.')
+        message, 'Please add a username to your account via Telegram settings\
+        for chat privacy and security.')
 
 
 # handle the bot request pipeline
 def do_user_action(username):
-    if username == "None" or username == None or username == "none":
+    if username == "None" or username is None or username == "none":
         return 0xDEADBEEF
     for i in total_users_chats:
         if i and i.username == username:
             return i
     else:
-        new_user = oif.OpenAIBot(init_msg, username)
+        new_user = oif.OpenAIBot(INIT_MSG, username)
         total_users_chats.append(new_user)
         return new_user
 
@@ -80,6 +79,7 @@ def getweatherinfo(message):
     logger.info(f'''<{message.from_user.username}>: {message.text}''')
 
 
+# Clear the user chat
 @bot.message_handler(commands=['clearchat'])
 def clearchat(message):
     ctx_user = do_user_action(str(message.from_user.username))
@@ -90,16 +90,15 @@ def clearchat(message):
         bot.reply_to(message, 'Cleared Chat')
         logger.info(f'''<{message.from_user.username}>: {message.text}''')
 
+
 # When user first starts a chat
-
-
 @bot.message_handler(commands=['start'])
 def initiate(message):
     ctx_user = do_user_action(str(message.from_user.username))
     if ctx_user == 0xDEADBEEF:
         no_username(bot, message)
     else:
-        bot.reply_to(message, user_init_text)
+        bot.reply_to(message, USER_INIT_TEXT)
         logger.info(f'''<{message.from_user.username}>: {message.text}''')
 
 
@@ -125,7 +124,8 @@ def chat_gpt_complete(message):
             logger.info(f'''<{message.from_user.username}>: {message.text}''')
         except openai.error.InvalidRequestError:
             bot.reply_to(
-                message, "You have run out of OpenAI messages, please run /clearchat to continue")
+                message, "You have run out of OpenAI messages, please run\
+                        /clearchat to continue")
 
 
 bot.infinity_polling()
